e781f9a6693c31f884a6512e54d8fce4d58044e9
malikmuhammadumair <numl-s24-13671@numls.edu.pk>
2026-03-06 08:32:02 +0500

    Delete src directory

--- src/main/java/cofee/example/cofee/Repository/AddDepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package cofee.example.cofee.Repository;
-
-import cofee.example.cofee.Entity.AddDepartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddDepartmentRepository extends JpaRepository<AddDepartment, Long> {
-}
